72890d9801eaaa7e88dd28aa086981ef4c2b2996
Ajaeb Ismael <ajaebabdurahman@outlook.com>
2019-05-20 15:19:47 -0400

    setting up the layout

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 //how to use array inside hash
-array = {arcanine: {name: 'arcanine', stat:{hp:10, attack:20}}}
+Pokemon_arr = []
+
 class Pokemon{
-  constructor(namePokemon){
-    this.newpokemon = array[namePokemon]
+  constructor(name, id, hp,){
+    this.name = name
     this.pokemonName = this.newpokemon.name
     this.hp = this.newpokemon.stat.hp
     this.attack = this.newpokemon.stat.attack
@@ -26,4 +27,26 @@ class Trainer{
   all(){
 
   }
+
+}
+
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     myObj = JSON.parse(this.responseText);
+     for (car in myObj) {
+       if(All_cars.length < 15){
+        let newCar = new Cars(car, myObj[car]['model'], myObj[car]['make'],myObj[car]['color'],  myObj[car]['model_year'])
+        All_cars.push(newCar)
+       }
+       if(Cars_order.length < 15){
+        let newCar = new Cars(car, myObj[car]['model'], myObj[car]['make'],myObj[car]['color'],  myObj[car]['model_year'])
+        Cars_order.push(newCar)
+       }
+     }
+    }
+  };
+  xhttp.open("GET", "https://raw.githubusercontent.com/mlaw-nycda/empireCommerce/master/cars.json", true);
+  xhttp.send();
 }
